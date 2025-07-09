918464e892bf7f9f3b3067b223220ea0fbd1d519
Mike <hungmanh2607@gmail.com>
2025-07-09 14:11:50 +0700

    Add gunicorn config and update start script

---
+++ emlinh_mng/gunicorn.conf.py
@@ -0,0 +1,6 @@
+bind = "0.0.0.0:5000"
+workers = 2
+timeout = 60
+loglevel = "info"
+errorlog = "gunicorn-error.log"
+accesslog = "gunicorn-access.log"
--- start_app.sh
+++ start_app.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -e
-cd /home/minipc/actions-runner/_work/emlinh-project/emlinh-project/emlinh_mng
 source venv/bin/activate
 exec python -m gunicorn -c gunicorn.conf.py wsgi:application
